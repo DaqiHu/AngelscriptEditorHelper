e700bd03518c371d0dd4b67f01287c48c5f849a2
DaqiHu <hudaqiwork@outlook.com>
2024-10-12 15:30:36 +0800

    init commit

---
+++ Source/AngelscriptEditorHelper/Private/ASLibrary.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2023-2024 Danta1ion.
+#include "ASLibrary.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UASLibrary::Print(const FString& InString, FLinearColor TextColor, float Duration, const FName Key)
+{
+    UKismetSystemLibrary::PrintString(GEditor->PlayWorld, InString, true, true, TextColor, Duration, Key);
+}
---
+++ Source/AngelscriptEditorHelper/Public/ASLibrary.h
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2023-2024 Danta1ion.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ASLibrary.generated.h"
+
+UCLASS()
+class ANGELSCRIPTEDITORHELPER_API UASLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    /** Helper function for editor scripts (Toolbox plugin or Editor Utility Widget) to print to viewport. */
+    UFUNCTION(BlueprintCallable)
+    static void Print(const FString& InString = FString(TEXT("Hello")), FLinearColor TextColor = FLinearColor(0.0f, 0.66f, 1.0f), float Duration = 5.f, const FName Key = NAME_None);
+};
